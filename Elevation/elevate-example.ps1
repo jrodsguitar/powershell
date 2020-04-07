f755fd77d5e7f40b29ea8ac30929929d5a40635e
jrodsguitar <jrodriguez@system76-pc.localdomain>
2020-04-06 20:18:02 -0700

    Added get-networkadapterinfo script

--- Elevation/elevate-example.ps1
+++ Elevation/elevate-example.ps1
@@ -1,21 +1,31 @@
+##How do you run a self elevating PowerShell script? 
+
+##I've often encountered situations where I had to run a self elevating PowerShell script for various reasons.
+
+
+##This script spawns a PowerShell process as an elevevated user (think UAC / Adminstrator elevation).##
+
+#-
 #Example of how to self elevate as Administrator(UAC) with alternate credentials.
 #Script by Jose Rodriguez
 #3/24/2020
-#Starts an elevated shell as an alternate user. Prompts for credentials.
+#
+
+#Starts an elevated shell as an alternate user and prompts for credentials.
 
 $username = Read-Host "Enter username"
 $password = Read-Host  -AsSecureString "Enter Password"
 $creds =   New-Object System.Management.Automation.PSCredential($username,$password)
 
 $runthis = {
-
-    #Run your commands here.
-
+    #Command that we intend to run after elevation.
 }
+#-
 
+##Now we take the credentials and command from above and we start a new powershell process in an elevated manner.##
 
-#If you're new to PowerShell this line will kick your ass.
-#Start a new powershell process with alternate credentials
-#Next it tells that process to start a new powershell process in an elevated manner.
-#
+##In essense, Start-Process calls powershell with given credentials. Then powershell calls Start-Process which calls powershell, which is what finally runs ours command. Confused yet? :) ##
+
+#-
 Start-Process powershell -Credential $creds -ArgumentList "-noprofile -command &{start-process powershell.exe {$runthis} -verb runas}"
+#-
---
+++ Network/get-networkadapterinfo.ps1
@@ -0,0 +1,22 @@
+##How to return only the IPv4 address from a remote computer##
+
+##In this example we use Windows Management Instrumentation infrastructure, to connect to a remote PC, and retrieves network information from the enabled NIC##
+
+##Description of each step is in the inline comments of the script below##
+
+#-
+##Computer to connect to##
+
+$computer = 'server123'
+
+##Utilize the WMI Win32_NetworkAdapterConfiguration class to return network adapter configuration. We filter on 'ipenabled'.##
+$networkAdapterInfo = (Get-WmiObject -CN $computer -Class Win32_NetworkAdapterConfiguration  -Filter 'ipenabled = "true"')
+    
+##This filters out the IPv6 address by using a regex pattern then replaces the 'ipaddress' field from our existing $networkAdapterInfo object with the IPv4 address##
+$networkAdapterInfo.ipaddress = $networkAdapterInfo.IPAddress.ForEach({($_ | select-string -pattern "(\d{1,3}(\.?)){4}")})
+    
+##Return the information we just gathered##
+$networkAdapterInfo
+#-
+
+##Sample Output:##
